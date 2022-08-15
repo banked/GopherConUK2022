bd313201186c93f8a49afb43b1e61775028b3156
Chris Reeves <hey@krak3n.io>
2022-08-15 12:12:52 +0100

    feat: add demo7 - managed mode

---
+++ demo7/README.md
@@ -0,0 +1,10 @@
+# Demo 6
+
+Using buf.build with managed mode.
+
+- `brew install bufbuild/buf/buf`
+- `brew install proto-gen-go`
+
+```bash
+buf generate
+```
---
+++ demo7/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	paymentv1 "github.com/banked/gopherconuk2022/demo7/banked/payment/v1"
+	bankedv1 "github.com/banked/gopherconuk2022/demo7/banked/v1"
+)
+
+func main() {
+	msg := paymentv1.ErrInsuffcientFunds{
+		PaymentId: "123",
+		Amount: &bankedv1.Amount{
+			Cents:    int64(10000),
+			Currency: "GBP",
+		},
+		Provider:    "Bank A",
+		AttemptedAt: timestamppb.New(time.Now().UTC()),
+	}
+
+	b, err := protojson.Marshal(&msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
+}
